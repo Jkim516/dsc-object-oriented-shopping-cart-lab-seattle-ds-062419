4d0fcb167e1f3c09d0fb71d54016ac7e408ae2bd
Jkim516 <Jungmokim516@gmail.com>
2019-07-22 18:15:39 -0700

    working on OOP shopping cart

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,46 @@
+
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
-    def add_item(self, name, price, quantity=1):
-       pass
+    def __init__(self, total=0, employee_discount=None, items=[]):
+        self.total = total
+        self.employee_discount = employee_discount
+        self.items = items
+        self.total_spent = sum([i['price']*i['quantity'] for i in items])
+#         self.discount_item = discount_item
+#         self.discount_price = sum([i['price'] for i in items])
+    def add_item(self, name=None, price=None, quantity=1):
+        if quantity == 1:
+            self.items.append({'name': name, 'price': price})
+        else:
+            for i in range(0, quantity):
+                self.items.append({'name': name, 'price': price})
+        self.total_spent += price * quantity
+        return self.total_spent
     def mean_item_price(self):
-       pass
+        x = [p['price'] for p in self.items] 
+        x.sort()
+        mean = sum(x) / len(x)
+        return mean
 
     def median_item_price(self):
-        pass
+        x = [p['price'] for p in self.items] 
+        x.sort()
+        half = len(x)//2
+        b = x[half]
+        c = x[-half-1]
+        median = (b + c) / 2
+        return median
 
     def apply_discount(self):
-       pass
-
+        x = [p['price'] for p in self.items] 
+        discount = sum(x) * .8
+        if discount != 0:
+            return discount
+        else:
+            return "sorry"
     def void_last_item(self):
         pass
+    def remove_item(self,list,index):
+        return list.pop(index)
+        
+       
